185aae2f6db102b6741d1d4e556146d28dd7653a
Centelles-Lodeiro-J <61518624+Centelles-Lodeiro-J@users.noreply.github.com>
2020-02-26 20:40:36 +0100

    Create LMRCTaxa.R

---
+++ LMRCTaxa.R
@@ -0,0 +1,20 @@
+Coef = c()
+Coef2 = c()
+Markers = c()
+
+
+for(i in x:length(data_marker)){
+  Coef[[i]] = tidy(anova(lmer(data_marker[[i]] ~ BMI_C + (1 | Comp.groups), 
+                              data = data_marker),
+                         lmer(data_marker[[i]] ~  (1 | Comp.groups), 
+                              data = data_marker)
+                         ,test = "Chisq"))[2,]
+  Coef2[[i]] =  tidy(lmer(data_marker[[i]] ~ BMI_C+ (1 | Comp.groups), 
+                          data = data_marker))
+  Coef2[[i]]$term[1] =  names(data_marker[i]) # Get taxa names
+  Markers[[i]]= Coef2[[i]][1,]
+}
+d = ldply(Coef, rbind) %>% mutate(q.value = p.adjust(ldply(Coef, rbind)[,"p.value"],method ="BH"))# Unlist and calculate q value BH
+Gen = ldply(Genera, rbind)
+GenTab <- cbind(Gen,d)
+SG =GenTab %>% arrange(by_group = estimate)
